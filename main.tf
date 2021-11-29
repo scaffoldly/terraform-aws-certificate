532239318ece209c3ee0cddd038877f50037b0ca
Christian Nuss <christian.nuss@gmail.com>
2021-11-29 09:59:12 -0800

    scaffoldly/certificate-stage/aws 1.0.4

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "certificate" {
   count = length(var.domains) > 0 ? 1 : 0
 
   source  = "scaffoldly/certificate-stage/aws"
-  version = "1.0.3"
+  version = "1.0.4"
 
   stage       = var.stage
   root_domain = var.root_domain
