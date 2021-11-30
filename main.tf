f6955e679ef0b6f147d8a022958be3d6e1711254
Christian Nuss <cnuss@users.noreply.github.com>
2021-11-29 20:25:18 -0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "certificate" {
   count = length(var.subdomains) > 0 ? 1 : 0
 
   source  = "scaffoldly/certificate-stage/aws"
-  version = "1.0.6"
+  version = "1.0.7"
 
   stage            = var.stage
   root_domain      = var.root_domain
